a91d651833ca038dfe90c52ec7fbfa9d08cabb38
Nguyen Tong Tran <ngtong1406@gmail.com>
2026-09-18 17:57:16 +0930

    Implement the Contact section

---
+++ src/components/ContactLinkLarge.jsx
@@ -0,0 +1,22 @@
+const ContactLinkLarge = ({ icon, type, username, link }) => {
+    return (
+        <a
+            href={link}
+            target="_blank"
+            className="w-auto max-h-50 overflow-hidden px-1 py-3 text-white bg-border-subbody border border-border-main hover:border-secondary hover:bg-border-sub transition-all duration-100 grid grid-cols-5 items-center gap-2"
+        >
+            <img
+                src={`https://api.iconify.design/${icon}.svg`}
+                alt={type + " " + username}
+                className="col-span-2 w-11 h-11 shrink-0 mx-auto"
+                loading="lazy"
+            />
+            <div className="col-span-3 flex flex-col">
+                <div className="font-semibold">{type}</div>
+                <div>{username}</div>
+            </div>
+        </a>
+    );
+};
+
+export default ContactLinkLarge;
--- src/pages/ContactSection.jsx
+++ src/pages/ContactSection.jsx
@@ -1,7 +1,65 @@
-import React from "react";
+import { ExternalLink } from "lucide-react";
+import contacts from "../data/contacts.json";
+import ContactLinkLarge from "../components/ContactLinkLarge";
 
 const ContactSection = () => {
-    return <div></div>;
+    const getGridColsClass = (length) => {
+        if (length % 2 === 0) return "grid-cols-2";
+        if (length % 3 === 0) return "grid-cols-3";
+        return "grid-cols-2";
+    };
+
+    return (
+        <>
+            <h1 className="section-heading">contact-details</h1>
+            <hr className="section-break" />
+            <div className="w-full my-20 flex flex-col items-center gap-4">
+                <div className="text-2xl text-white font-semibold font-serif">
+                    Let's build something{" "}
+                    <span className="underline underline-offset-4">
+                        together
+                    </span>
+                    .
+                </div>
+                <div>
+                    Whether you have a specific role in mind or just want to
+                    discuss a project,{" "}
+                    <a
+                        className="inline-flex items-center gap-2 text-white hover:underline hover:underline-offset-4"
+                        target="_blank"
+                        href="https://mail.google.com/mail/?view=cm&fs=1&to=ngtong1406@gmail.com&su=Inquiry+from+Portfolio"
+                    >
+                        <span>drop me an email</span>
+                        <ExternalLink className="w-4 h-4 shrink-0 -translate-y-0.5" />
+                    </a>
+                    — I'm always open to new ideas. 👍
+                </div>
+                <div className="w-full grid grid-cols-5 gap-4 my-10">
+                    <hr className="col-span-2 section-break" />
+                    <div className="col-span-1 text-center">
+                        Otherwise, you can easily find me on...
+                    </div>
+                    <hr className="col-span-2 section-break" />
+                </div>
+                <div
+                    className={
+                        "w-1/4 grid gap-4 " + getGridColsClass(contacts.length)
+                    }
+                >
+                    {contacts.map((contact) => (
+                        <div key={contact.type} className="col-span-1">
+                            <ContactLinkLarge
+                                icon={contact.icon}
+                                type={contact.type}
+                                username={contact.username}
+                                link={contact.url}
+                            />
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default ContactSection;
