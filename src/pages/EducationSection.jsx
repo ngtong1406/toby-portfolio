0933b6a261182f2704bf6094ab3f35de1120e9fb
Nguyen Tong Tran <trany056@mymail.unisa.edu.au>
2026-09-18 19:18:11 +0930

    Improve responsive behaviour for two large texts.

--- src/pages/EducationSection.jsx
+++ src/pages/EducationSection.jsx
@@ -5,21 +5,15 @@ const EducationSection = () => {
     return (
         <div className="w-full grid grid-cols-7 gap-17">
             <div className="w-full col-span-3">
-                <img
-                    className="w-full h-auto object-cover"
-                    src={pridhamHallPhoto}
-                    alt=""
-                />
+                <img className="w-full h-auto object-cover" src={pridhamHallPhoto} alt="" />
             </div>
             <div className="col-span-4 flex flex-col">
                 <div className="h-1/3 flex flex-col items-start">
-                    <div className="text-2xl">
-                        GRADUATION CEREMONY // FEB. 2026
-                    </div>
+                    <div className="text-lg xl:text-2xl">GRADUATION CEREMONY // FEB. 2026</div>
                     <a
                         href="https://adelaide.edu.au/"
                         target="_blank"
-                        className="font-serif text-white font-bold text-9xl hover:underline"
+                        className="font-serif text-white font-bold text-6xl lg:text-7xl xl:text-8xl 2xl:text-9xl hover:underline"
                     >
                         @UniSA
                     </a>
