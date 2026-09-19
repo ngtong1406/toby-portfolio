feb4b8e19daa92a54036e25d31b921dee5501a72
Nguyen Tong Tran <trany056@mymail.unisa.edu.au>
2026-09-20 02:51:46 +0930

    Make year dynamically update in real time.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,12 @@
 import PreciseAge from "./PreciseAge";
 
+const CURRENT_YEAR = new Date().getFullYear();
+
 const Footer = () => {
     return (
         <footer className="w-full flex flex-col gap-3 mb-10 items-center justify-center">
             <hr className="section-break" />
-            <div>© 2026 Toby Tran. All rights reserved.</div>
+            <div>© {CURRENT_YEAR} Toby Tran. All rights reserved.</div>
             <PreciseAge />
         </footer>
     );
